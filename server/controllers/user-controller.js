31d42c6e9e9a5f55bc049e22618ebf22baf4f300
Lensom <magurawork@gmail.com>
2021-09-25 20:00:55 +0300

    Add confirm email

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,14 @@
+const userService = require('../service/user-service');
+
 class UserController {
   async registration(req, res, next) {
     try {
-
+      const {email, password} = req.body;
+      const userData = await userService.registration(email, password);
+      res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+      return res.json(userData);
     } catch (e) {
-
+      console.log(e)
     }
   }
 
@@ -25,7 +30,9 @@ class UserController {
 
   async activate(req, res, next) {
     try {
-      
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL);
     } catch (e) {
 
     }
@@ -43,7 +50,7 @@ class UserController {
     try {
       res.json(['123', '123']);
     } catch (e) {
-
+      console.log(e);
     }
   }
 
